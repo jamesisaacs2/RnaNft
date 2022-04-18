f4a672d9a81037062b9fc1951b2ead6deebdfaa9
James Isaacs <jkifao@gmail.com>
2022-04-18 00:36:18 -0700

    header and data assets

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,10 +1,27 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
+
+import { COLORS, NftData } from "../constants/indexAssets";
+import { HomeHeader, FocusedStatusBar, NftCard } from "../components/indexComponents";
 
 const Home = () => {
 	return (
 		<>
-			<Text>Home</Text>
-			<Text>yes</Text>
+			<SafeAreaView style={{ flex: 1 }}>
+				<Text>Home</Text>
+				<FocusedStatusBar background={COLORS.primary} />
+				<View style={{ flex: 1 }}>
+					<View style={{ zIndex: 0 }}>
+						<FlatList
+							data={NftData}
+							renderItem={({ item }) => <Text>{item.name}</Text>}
+							keyExtractor={(item) => item.id}
+							showsVerticalScrollIndicator={false}
+							ListHeaderComponent={<HomeHeader />}
+						/>
+					</View>
+				</View>
+			</SafeAreaView>
 		</>
 	);
 };
